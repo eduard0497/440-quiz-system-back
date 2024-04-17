420374c33b0e1cb3da6710dc9cc1024132ef0c90
Eduard <edhovhannisyan04@gmail.com>
2024-04-16 23:22:37 -0700

    added clear all functionality

--- index.js
+++ index.js
@@ -570,7 +570,8 @@ app.post("/teacher-get-submissions-for-quiz", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("APP IS RUNNING ON PORT 3000");
 });
 
